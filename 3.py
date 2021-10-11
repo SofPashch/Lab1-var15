dba28d08e9f6db58bfcdcfef91df440a68cc5227
SofPashch <92335898+SofPashch@users.noreply.github.com>
2021-10-11 23:30:41 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,10 +1,15 @@
-a = [3,5,8,-4]
+a=[]
+n=int(input("Введіть розмір масиву:"))
+print("Введіть елементи масиву:")
+for i in range(n):
+    a.append(int(input()))
+print("Масив:")
+print(a)
 answer = 1
-#
+
 print("Мінімальний елемент:")
 print(min(a))
 
-#
 print("Ненульові непарні елементи масиву:")
 for i in range(len(a)):
     if(a[i]>0 and a[i]%2>0):
@@ -14,7 +19,6 @@ for i in range(len(a)):
 print("Їх добуток:")
 print(answer)
 
-#
 a.reverse()
 print("Масив у зворотньому порядку:")
 print(a)
